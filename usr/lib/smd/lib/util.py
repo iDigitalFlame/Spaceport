e53a81a97a74d1ff9fc14cd635b7f07bb4dc7004
iDigitalFlame <idf@idigitalflame.com>
2021-12-09 12:45:32 -0500

    Year Update

--- usr/lib/smd/lib/util.py
+++ usr/lib/smd/lib/util.py
@@ -5,7 +5,7 @@
 #
 # System Management Daemon
 #
-# Copyright (C) 2016 - 2021 iDigitalFlame
+# Copyright (C) 2016 - 2022 iDigitalFlame
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
